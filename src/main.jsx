4fe9be9726e65a4994859a9da76fae27921af990
youngjinjung <jyj1641@gmail.com>
2024-10-30 14:11:30 +0900

    TailwindCSS 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './AppStyled.jsx';
+import App from './AppTailwindCSS.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
