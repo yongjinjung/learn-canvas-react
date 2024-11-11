d27f771be70d7fd4acd1fd5692744ee6b9e78aa2
youngjinjung <jyj1641@gmail.com>
2024-11-11 14:49:38 +0900

    검색 필터 리뉴얼

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,16 +10,27 @@ import Error from '../components/Error';
 import Button from '../components/Button';
 import useApiRequest from '../hooks/useApiRequest';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import CategoryFilter from '../components/CategoryFilter';
 
 export default function Home() {
-  const [searchText, setSearchText] = useState();
+  const [filter, setFilter] = useState({
+    searchText: undefined,
+    category: undefined,
+  });
+  const handleFilter = (key, value) =>
+    setFilter({
+      ...filter,
+      [key]: value,
+    });
+
   const [isGridView, setIsGridView] = useState(true);
 
   const queryClient = useQueryClient();
   // 1] 데이터 조회
   const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ['canvases', searchText],
-    queryFn: () => getCanvases({ title_like: searchText }),
+    queryKey: ['canvases', filter.searchText, filter.category],
+    queryFn: () =>
+      getCanvases({ title_like: filter.searchText, tag: filter.category }),
     initialData: [],
   });
 
@@ -50,7 +61,16 @@ export default function Home() {
   return (
     <>
       <div className="mb-6 flex flex-col sm:flex-row data-center justify-between">
-        <SearchBar searchText={searchText} setSearchText={setSearchText} />
+        <div className="flex gap-2 flex-col w-full sm:flex-row mb-4 sm:mb-0">
+          <SearchBar
+            searchText={filter.searchText}
+            onSearch={val => handleFilter('searchText', val)}
+          />
+          <CategoryFilter
+            category={filter.category}
+            onChange={val => handleFilter('category', val)}
+          />
+        </div>
         <ListType isGridView={isGridView} setIsGridView={setIsGridView} />
       </div>
       <div className="flex justify-end mb-6">
@@ -68,7 +88,7 @@ export default function Home() {
       {!isLoading && !error && (
         <CanvasList
           filteredData={data}
-          searchText={searchText}
+          searchText={filter.searchText}
           isGridView={isGridView}
           onDeleteItem={handleDeleteItem}
         />
